5696cc69fc4b2e9e5ff89a1958ef7831b90aa9c7
Ashikur_efty01 <ashikurefty713@gmail.com>
2024-11-09 00:23:02 +0600

    added

---
+++ array-loops.js
@@ -0,0 +1,6 @@
+var numbers= [45, 87, 56, 32, 51, 25];
+
+for ( var i=0; i<7; i++){
+    var number = numbers[i];
+    console.log(number);
+}
---
+++ for.js
@@ -0,0 +1,8 @@
+// var roastGiven = 0;
+// while(roastGiven <7){
+//     console.log('Roast den , please !!');
+//     roastGiven++;
+// }
+for(var roastGiven = 0; roastGiven < 7 ; roastGiven++){
+    console.log('Roast Den , please !!');
+}
